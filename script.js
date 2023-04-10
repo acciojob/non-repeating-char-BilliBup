6c0bb893e5a7dc8d8e2f29dcab45bd319a6b3df6
SHRISHTY MALVIYA <103518988+BilliBup@users.noreply.github.com>
2023-04-10 18:58:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 function charachterRepeat(str) {
-	for(int i = 0; i < str.length(); i++){
+	for(var i = 0; i < str.length(); i++){
 		if(str.indexOf(str.charAt(i))==str.lastIndexOf(str.charAt(i)){
 			return str.charAt(i);
 		}
