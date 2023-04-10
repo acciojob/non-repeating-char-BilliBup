1169a0c28acb087b6e486aeb3022f43e6f9b35b7
SHRISHTY MALVIYA <103518988+BilliBup@users.noreply.github.com>
2023-04-10 19:04:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //your JS code here. If required.
-function charachterRepeat(str) {
-	for(var i = 0; i < str.length; i++){
-		if(str.indexOf(str.charAt(i))==str.lastIndexOf(str.charAt(i)){
-			return str.charAt(i);
-		}
-	}
-	
+function characterRepeat(str) {
+  for (var i = 0; i < str.length; i++) {
+    if (str.indexOf(str.charAt(i)) == str.lastIndexOf(str.charAt(i))) {
+      return str.charAt(i);
+    }
+  }
 }
